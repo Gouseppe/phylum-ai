eae097e4e635a5a04b50434c5af13a953ad0b37e
Gouseppe <fernandeitorc@gmail.com>
2025-01-10 11:48:07 -0400

    feat: implement UserHelper.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import bgVideo from "@/assets/bg/video/bacterias.mp4";
-import Button from "./components/Button";
 import { QUESTIONS } from "@/config/index";
-import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
-import { Label } from "./components/ui/label";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useMotorDeInferencia } from "./hooks/phylumAi";
 import { getPhylum } from "./config/api/backend/requests/phylum";
+import bgVideo from "@/assets/bg/video/bacterias.mp4";
+// hooks
+import { useMotorDeInferencia } from "./hooks/phylumAi";
+// icons
+import { ChevronLeft, ChevronRight } from "lucide-react";
+// components
+import { RadioGroup, RadioGroupItem } from "./components/shadcn/radio-group";
+import { Label } from "./components/shadcn/label";
+
+import Button from "./components/owner/Button";
+import { UserHelper } from "./components/owner/UserHelper";
 
 function App() {
   const {
@@ -67,7 +72,7 @@ function App() {
       ></video>
       <div className="flex md:flex-row flex-col items-start gap-4 w-full md:justify-center md:h-auto h-full">
         {/* contenedor de las preguntas respondidas */}
-        <div className="bg-slate-50 flex flex-col rounded-md overflow-auto md:-order-none order-1">
+        <div className=" flex flex-col rounded-md overflow-auto md:-order-none order-1 p-4 bg-black bg-opacity-30 gap-2">
           {questions.map((value) => {
             return (
               <div
@@ -75,7 +80,7 @@ function App() {
                 onClick={() => {
                   goToQuestion(value);
                 }}
-                className="p-2 cursor-pointer hover:bg-slate-500"
+                className="p-2 cursor-pointer hover:bg-slate-500 bg-white rounded-sm"
               >
                 <div>pregunta: {QUESTIONS[Number(value)].text}</div>
                 <div>
@@ -90,22 +95,37 @@ function App() {
         </div>
         {/* contenedor de la pregunta actual */}
         <div className="flex flex-col gap-4 md:w-3/4 md:max-w-xl w-full">
-          <div className="flex bg-slate-50 rounded-md p-4">
+          <div className="flex flex-col bg-slate-50 rounded-md p-4">
             {QUESTIONS[preguntaActual]?.text || "nada"}
+            {QUESTIONS[preguntaActual].userHelper?.description && (
+              <div className="item self-end">
+                <UserHelper
+                  description={QUESTIONS[preguntaActual].userHelper.description}
+                  title={QUESTIONS[preguntaActual].userHelper.title}
+                />
+              </div>
+            )}
           </div>
-          <div className="bg-slate-50 rounded-md p-4">
+          <div className="flex flex-col bg-slate-50 rounded-md p-4 gap-3">
             <div>
               <RadioGroup
                 value={String(answers[preguntaActual])}
                 onValueChange={handleRespuestaSeleccionada}
+                className="gap-0"
               >
                 {QUESTIONS[preguntaActual].Options.map((opcion, index) => (
-                  <div key={index} className="flex items-center space-x-2 mb-4">
+                  <div key={index} className="flex items-center space-x-2 ">
                     <RadioGroupItem
                       value={String(opcion.value)}
                       id={`opcion-${index}`}
                     />
                     <Label htmlFor={`opcion-${index}`}>{opcion.text}</Label>
+                    {opcion.userHelper && (
+                      <UserHelper
+                        description={opcion.userHelper.description}
+                        title={opcion.userHelper.title}
+                      />
+                    )}
                   </div>
                 ))}
               </RadioGroup>

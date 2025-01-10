e022cf1f224d14f75e23d8817473640536e0bf33
Gouseppe <fernandeitorc@gmail.com>
2025-01-10 11:49:13 -0400

    fix: add userHelper prop to option and Question Type

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,9 +1,15 @@
 export type Option = {
   text: string;
   value: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  userHelper?: UserHelper;
+};
+export type UserHelper = {
+  title: string;
+  description: string;
 };
 
 export type Question = {
   text: string;
   Options: Option[];
+  userHelper?: UserHelper;
 };
